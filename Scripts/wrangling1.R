6484ee6b3abe67e1d07e3e73af08458444c56afb
BRAD SMITH <bradley.carl.smith@gmail.com>
2015-09-23 11:15:54 -0400

    Update to post-processing script
    Transforms Rstan output into a data frame for use in analysis and
    visualization.

--- Scripts/wrangling1.R
+++ Scripts/wrangling1.R
@@ -16,7 +16,8 @@ require(foreign)
 
 ### Set WD, load in Rstan results
 setwd("~/Google Drive/Research/IO_latent.engage")
-load("Data/engagement_10k_3chain.RData")
+load("Output/UNHRCfit.RData")
+load("Data/engagement_v3.RData")
 
 ### Load in full dataset to extract ID information (e.g. year, COW code)
 data <- read.dta("Data/IOfull.dta")
@@ -27,10 +28,24 @@ ID.data <- unique(eng[c("Country","Year","COWid")])
 ID.data$EngID <- ID.data$Year*1000+ID.data$COWid
 
 # Bind means from estimation to Engagement ID values
-thetas <- est$theta
-thetas <- colMeans(thetas,dims=1)
+thetas <- UNHRCfit$theta
+theta <- colMeans(thetas,dims=1)
+theta95pct <- apply(thetas,
+                     2,
+                     quantile,
+                     probs = c(0.025, 0.975))
+theta95pct <- as.data.frame(cbind(theta95pct[1,], theta95pct[2,]))
+names(theta95pct) <- c("theta95lower", "theta95upper")
+theta80pct <- apply(thetas,
+                    2,
+                    quantile,
+                    probs = c(0.1, 0.9))
+theta80pct <- as.data.frame(cbind(theta80pct[1,], theta80pct[2,]))
+names(theta80pct) <- c("theta80lower", "theta80upper")
 thetas <- as.data.frame(cbind(EngID,
-                              thetas))
+                              theta,
+                              theta95pct,
+                              theta80pct))
 
 # Merge the full data with the theta means
 output.data <- merge(ID.data,
@@ -38,7 +53,7 @@ output.data <- merge(ID.data,
                      by = intersect(names(ID.data),names(thetas)))
 
 rm(list=setdiff(ls(),"output.data"))
-
+save.image("~/Google Drive/Research/IO_latent.engage/Output/output.data.RData")
 
 
 
