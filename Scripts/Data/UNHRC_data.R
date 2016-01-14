f329a19293f4f8e05be4c5663940805615900408
GFJudd33 <gleason.f.judd@gmail.com>
2016-01-14 01:35:59 -0500

    change full.dta back to full.uniq for clarity

--- Scripts/Data/UNHRC_data.R
+++ Scripts/Data/UNHRC_data.R
@@ -15,14 +15,14 @@ library(foreign)
 full.dta <- read.dta("IOfull.dta")
 
 #remove `duplicate' communications (i.e. same country, response, and dates)
-full.dta <- full.dta[!duplicated(full.dta[c(1,6:8)]),]
+full.uniq <- full.dta[!duplicated(full.dta[c(1,6:8)]),]
 
 #select variables of interest for response models.
-attach(full.dta)
+attach(full.uniq)
 fullX <- data.frame(cbind(Country, Year, COWid, BinQualCode, StateDept, pop, rgdpl2, AL, JAL, UA, JUA, StInvite, 
            CountryMandate, CtryVisit, polity2, UNHRC, ED, FOOD,	FRDX,	HLTH,
            HOUSE, HRD, IJL, IND, MIG, RACE, RINT, SALE, SUMX, TOR, TOX, TRAF,	VAW))
-detach(full.dta)
+detach(full.uniq)
 
 #read in country year dataset.
 ctryyear <- read.dta("CtryYearIO.dta")
