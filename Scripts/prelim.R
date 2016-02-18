2bf1dbf33a63bad864877e4b19b190920756607d
BRAD SMITH <bradley.carl.smith@gmail.com>
2016-02-18 09:06:20 -0500

    IIA test
    Adds IIA test, which rejects assumption for omission of “in
    translation” choice.

--- Scripts/prelim.R
+++ Scripts/prelim.R
@@ -18,7 +18,6 @@ full <- read.dta("IOfull.dta")
 # Define choice variable after removing missing responses
 full.uniq <- full.uniq[is.na(full.uniq$QualCode) == FALSE,]
 full.uniq$choice <- factor(full.uniq$QualCode)
-#full.uniq <- full.uniq[full.uniq$TORT == 1,]
 
 data <- mlogit.data(full, shape = "wide", choice = "QualCode")
 
@@ -32,13 +31,23 @@ data <- as.matrix(data)
 data <- as.data.frame(data)
 row.names(data) <- c(1:nrow(data))
 shape <- mlogit.data(data, shape = "wide", choice = "choice")
-mnl <- mlogit(choice ~ 0 | CountryMandate + StInvite + rgdpl,
+mnl <- mlogit(choice ~ 0 | StInvite,
+              reflevel = 1,
               data = shape)
 summary(mnl)
 
 
 # Hausman test for IIA
+# We'll remove the in translation option and see how the others change
+mnl2 <- mlogit(choice ~ 0 | StInvite,
+               reflevel = 1,
+               data = shape,
+               alt.subset = c(1,2,3,4,5))
+summary(mnl2)
 
+# IIA is rejected. Note that we had to remove Country Invite b/c of 
+# singularity
+hmftest(mnl, mnl2)
 
 # Write separate script for mixed logit because it might take a little while
 # to run. 
